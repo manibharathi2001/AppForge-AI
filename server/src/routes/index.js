4d27ac5daca710606b29215b1f992f08ced12ecd
manibharathi2001 <manibharathi8005@gmail.com>
2026-08-01 19:15:29 +0530

    Add project CRUD backend

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import authRoutes from './auth.routes.js';
     
 router.use('/auth', authRoutes);
+router.use('/projects', projectRoutes);
 
 const router = Router();
 
